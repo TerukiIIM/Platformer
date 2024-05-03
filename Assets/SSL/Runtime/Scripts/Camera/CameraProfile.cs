7f0715e15f2b6843d15f9a125359cca4b8f43734
TerukiTogami <147390861+TerukiIIM@users.noreply.github.com>
2024-05-03 10:14:38 +0200

    update

--- Assets/SSL/Runtime/Scripts/Camera/CameraProfile.cs
+++ Assets/SSL/Runtime/Scripts/Camera/CameraProfile.cs
@@ -1,11 +1,21 @@
 ﻿using UnityEngine;
 
 public class CameraProfile : MonoBehaviour {
+    [Header("Type")]
+    [SerializeField] private CameraProfileType _profileType = CameraProfileType.Static;
+    
+    [Header("Follow")]
+    [SerializeField] private Transform _targetToFollow = null;
+
     private Camera _camera;
 
     public float CameraSize => _camera.orthographicSize;
 
     public Vector3 Position => _camera.transform.position;
+
+    public CameraProfileType ProfileType => _profileType;
+
+    public Transform TargetToFollow => _targetToFollow;
 
     private void Awake() {
         _camera = GetComponent<Camera>();
@@ -13,5 +23,10 @@ public class CameraProfile : MonoBehaviour {
             _camera.enabled = false;
         }
     }
+}
+
+public enum CameraProfileType {
+    Static = 0,
+    FollowTarget
 }
 
